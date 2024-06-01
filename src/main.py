22f1b3c983137b59cf4ddc45fe9c8e71b1dab852
Utkarsh Rastogi <utkarshrastogi081@gmail.com>
2024-06-02 00:55:04 +0530

    initial commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from s3_hook import S3
+from snf_hook import Snf
+
+if __name__ == "__main__":
+    print("hello world")
---
+++ src/s3_hook.py
@@ -0,0 +1,15 @@
+import boto3
+
+
+
+class S3:
+
+    #setup bucket name
+    #file name
+
+    #connect to s3 bucket
+
+    #list file method
+
+    # get the file method
+    pass
